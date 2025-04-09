364ab9fe1068f6d1a86a02ad2a26f0cfb91a50c8
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2025-04-09 17:25:56 +0300

    Перший комміт

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, Text } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return <View><Text>Гра</Text></View>;
+}
+
+function TasksScreen() {
+  return <View><Text>Завдання</Text></View>;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Гра" component={HomeScreen} />
+        <Tab.Screen name="Завдання" component={TasksScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
